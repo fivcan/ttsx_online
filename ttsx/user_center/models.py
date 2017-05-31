04bf5b7109cf0825ad998747989655bf37dc696c
fivcan <fivcan@163.com>
2017-05-31 11:19:39 +0800

    修改register.html

--- ttsx/user_center/models.py
+++ ttsx/user_center/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    user = models.CharField(max_length=20)
+    pwd = models.CharField(max_length=20)
+    mail = models.CharField(max_length=50)
+    address = models.CharField(max_length=40, default='')
+    receiver = models.CharField(max_length=20, default='')
+    postcod = models.IntegerField(default='')
+    tel = models.CharField(max_length='20',default='')
